b17f62a841b47de8c4df828caa7dc90602ecf260
Adrian Iacuone <aiacuone@gmail.com>
2025-01-25 15:23:00 +1000

    removed more comments

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,6 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'raw.githubusercontent.com',
-        // port: '',
-        // pathname: '/account123/**',
-        // search: '',
       },
     ],
   },
