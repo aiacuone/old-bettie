72ffcd3afbd491d307d8c9fe74d3dd9490f21e9b
Adrian Iacuone <aiacuone@gmail.com>
2025-01-25 14:42:48 +1000

    homepage image and layout issue resolved

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center text-center items-center h-[200px] gap-5 w-full mx-auto">
+    <main className="flex flex-col justify-center text-center items-center gap-5 w-full mx-auto">
       <h1>Home</h1>
       <Link href="/pokemon" className="text-blue-400">
         View Pokemon!!!
@@ -12,7 +12,7 @@ export default function Home() {
         Pokémon is a series of video games developed by Game Freak and published
         by Nintendo and The Pokémon Company under the Pokémon media franchise
       </p>
-      <Image width={200} height={200} src="/public/char.png" alt="Charmander" />
+      <Image width={200} height={200} src="/char.png" alt="Charmander" />
     </main>
   )
 }
